8b1d6fa75dd18377d9f4b916017bfb1683ca2299
Sheikh Mohammad Rifat <brotherssk57@gmail.com>
2021-08-29 21:19:21 +0600

    removed key and updated js

--- script.js
+++ script.js
@@ -12,39 +12,49 @@ const loadData = () => {
 
 const weatherDetails = (weather) => {
     console.log(weather);
+    
     const weatherDes = document.getElementById("weather-status");
+    
     weatherDes.textContent = "";
 
 
     // error handling
-    if(weather.cod=="404" || weather.cod=="400"){
+    if(weather.cod=="404" || weather.cod=="400")
+    {
+        
        weatherDes.innerHTML=`<p> Please enter a valid city name! <p>`
     }
 
 
     // weather icon
     let img = document.createElement("img");
+    
     img.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    
     weatherDes.appendChild(img);
 
     // weather city name
     let h1CityName = document.createElement("h1");
+    
     h1CityName.innerText = `Weather in ${weather.name}(${weather.sys.country})`;
+    
     weatherDes.appendChild(h1CityName);
 
     // temperature
     let h3Temp = document.createElement("h3");
+    
     h3Temp.innerText = `${((weather.main.temp)-273.14).toFixed(2)}°C`;
+    
     weatherDes.appendChild(h3Temp);
 
     // weather description
     let h1WeatherDetail = document.createElement("h1");
 
     h1WeatherDetail.innerText = "";
+    
     h1WeatherDetail.innerText = `${weather.weather[0].main}`;
 
     weatherDes.appendChild(h1WeatherDetail);
 
-
-
-}
+    
+};
